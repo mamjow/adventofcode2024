e24e45beca2f9c21ee3149e15dce546859a4bad8
M.J. Moshiri <mohammad.moshiri@cadmes.com>
2024-11-28 13:00:30 +0100

    snd commit

--- Program.cs
+++ Program.cs
@@ -2,5 +2,23 @@
 using Days;
 using AdventOfCode;
 
-AdventOfCodeChallenge.SolveEventChallenge(new Day4());
-Console.ReadLine();
+
+        Console.WriteLine("Enter a day (1-29): ");
+        if (int.TryParse(Console.ReadLine(), out int dayNumber) && dayNumber >= 1 && dayNumber <= 29)
+        {
+            ISolve solution = GetDayInstance(dayNumber);
+            if (solution != null)
+            {
+                AdventOfCodeChallenge.SolveEventChallenge(solution);
+            }
+            else
+            {
+                Console.WriteLine("Solution not found for the entered day.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number between 1 and 29.");
+        }
+
+        Console.ReadLine();
