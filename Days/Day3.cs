7e56a161e4b9a40ec7cee4ff5f4d8eb1fc7d6218
M.J. <mj.moshiri@gmail.com>
2024-12-03 19:36:02 +0100

    day3

--- Days/Day3.cs
+++ Days/Day3.cs
@@ -4,14 +4,15 @@ namespace Days;
 
 public class Day3 : ISolve
 {
-    string regexPattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+    string mulPattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+    string controlPattern = @"do\(\)|don't\(\)";
     public string SolvePartOne(string[] input)
     {
         var parsedNumbers = new List<int>();
 
         foreach (var line in input)
         {
-            var numbers = Regex.Matches(line, regexPattern);
+            var numbers = Regex.Matches(line, mulPattern);
 
             foreach (Match item in numbers)
             {
@@ -25,6 +26,43 @@ public class Day3 : ISolve
     }
     public string SolvePartTwo(string[] input)
     {
-        throw new NotImplementedException();
+        var parsedNumbers = new List<int>();
+        bool acceptMatches = true;
+        foreach (var line in input)
+        {
+            // Find matches for both mul() and control patterns
+            MatchCollection controls = Regex.Matches(line, controlPattern);
+            MatchCollection mulMatches = Regex.Matches(line, mulPattern);
+            // Process matches
+            int lastControlIndex = 0;
+            foreach (Match mulMatch in mulMatches)
+            {
+                // Check if there was a control match before the current mul()
+                while (lastControlIndex < controls.Count && controls[lastControlIndex].Index < mulMatch.Index)
+                {
+                    string controlValue = controls[lastControlIndex].Value;
+
+                    if (controlValue == "do()")
+                    {
+                        acceptMatches = true;
+                    }
+                    else if (controlValue == "don't()")
+                    {
+                        acceptMatches = false;
+                    }
+
+                    lastControlIndex++;
+                }
+
+                if (acceptMatches)
+                {
+                    _ = int.TryParse(mulMatch.Groups[1].ToString(), out int num1);
+                    _ = int.TryParse(mulMatch.Groups[2].ToString(), out int num2);
+                    parsedNumbers.Add(num1 * num2);
+                }
+            }
+
+        }
+        return parsedNumbers.Sum().ToString();
     }
 }
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 |---|---|---|---|
 |[Day 1: Historian Hysteria](https://adventofcode.com/2024/day/1)    | [🔑](https://github.com/mamjow/adventofcode2024/blob/main/Days/Day1.cs)   | C#       | :star: :star: |           
 |[Day 2: Red-Nosed Reports](https://adventofcode.com/2024/day/2)    | [🔑](https://github.com/mamjow/adventofcode2024/blob/main/Days/Day2.cs)   | C#       | :star: :star: |   
+|[Day 3: Mull It Over](https://adventofcode.com/2024/day/3)    | [🔑](https://github.com/mamjow/adventofcode2024/blob/main/Days/Day3.cs)   | C#       | :star: :star: |   
+
+![s](img_00001.svg)
